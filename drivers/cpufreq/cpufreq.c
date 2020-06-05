5e183d7e48f7b1cfacb91f5b2a938aa655b1dbbd
kazudante89 <karlemmanueldorvil@gmail.com>
2020-06-05 09:02:36 -0400

    cpufreq: Don't validate cpufreq table from cpufreq_generic_init()
    The cpufreq table is already validated by the cpufreq core and none of
    the users of cpufreq_generic_init() have any dependency on it to
    validate the table as well.
    
    Don't validate the cpufreq table anymore from cpufreq_generic_init().
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -236,15 +236,9 @@ EXPORT_SYMBOL_GPL(get_cpu_idle_time);
 int cpufreq_generic_init(struct cpufreq_policy *policy,
 		struct cpufreq_frequency_table *table,
 		unsigned int transition_latency)
+
 {
-	int ret;
-
-	ret = cpufreq_table_validate_and_show(policy, table);
-	if (ret) {
-		pr_err("%s: invalid frequency table: %d\n", __func__, ret);
-		return ret;
-	}
-
+  policy->freq_table = table;
 	policy->cpuinfo.transition_latency = transition_latency;
 
 	/*
